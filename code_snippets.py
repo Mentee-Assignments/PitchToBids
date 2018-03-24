b642e7bf0431fe26f894e1a8efba185214211a94
TrevorLeeCline <trvrcln@gmail.com>
2018-03-24 16:55:42 -0500

    [ENH] filled the pattern and repl variables with the appropriate regular expressions

--- code_snippets.py
+++ code_snippets.py
@@ -16,21 +16,20 @@ file_dict['flankers'] = glob(path + prefix + '/Flanker/run[1-2]/Flanker[1-2]Raw.
 
 file_dict['rests'] = glob(path + prefix + '/rsOut/func/RestingStateRaw.nii.gz')
 
-asdf = 5
 
 for scantype, filenames in file_dict.items():
     if scantype == 't1s':
-        pattern = re.compile(r"")
-        repl = r""
-        #
+        pattern = re.compile(r"^.*PreprocData/([0-9]{2})/([CEa-z]*).*$")
+        repl = r"sub-0\1/ses-\2Pre/anat/sub-0\1_ses-\2Pre_T1w.nii.gz"
+        #sub-001/ses-ControlPre/anat/sub-001_ses-ControlPre_T1w.nii.gz
     elif scantype == 'asls':
-        pattern = re.compile(r"")
-        repl = r""
-        #
+        pattern = re.compile(r"^.*PreprocData/([0-9]{2})/([Cea-z]*)/([Preost]*).*$")
+        repl = r"sub-0\1/ses-\2\3/func/sub-0\1_ses-\2\3_asl.nii.gz"
+        #sub-001/ses-ControlPre/func/sub-001_ses-ControlPre_asl.nii.gz
     elif scantype == 'flankers':
-        pattern = re.compile(r"")
-        repl = r""
-        #
+        pattern = re.compile(r"^.*PreprocData/([0-9]{2})/([Cea-z]*)/([Preost]*)/Flanker/run([1-2]).*$")
+        repl = r"sub0\1/ses-\2\3/func/sub-0\1_ses-\2\3_task-flanker_run-0\4_bold.nii.gz"
+        #sub-001/ses-ControlPre/func/sub-001_ses-ControlPre_task-flanker_run-01_bold.nii.gz
     elif scantype == 'rests':
         pattern = re.compile(r"^.*PreprocData/([0-9]{2})/([CEa-z]*)/([Preost]*).*$")
         repl = r"sub-0\1/ses-\2\3/func/sub-0\1_ses-\2\3_task-rest_bold.nii.gz"
